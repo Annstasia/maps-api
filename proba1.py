623cf194c1e6ec45cec4a3e882b020f11b61160d
Annstasia <45208486+Annstasia@users.noreply.github.com>
2019-04-08 20:25:03 +0300

    Часть 1

---
+++ proba1.py
@@ -0,0 +1,40 @@
+import pygame
+import requests
+import sys
+import os
+try:
+    map_request = "http://static-maps.yandex.ru/1.x/"
+    params = {
+        'll': '100,65',
+        'spn': '30,30',
+        'l': 'map'
+    }
+    response = requests.get(map_request, params=params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+except:
+    print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+os.remove(map_file)
